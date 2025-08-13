b1ebfb234d1024c34ece27a43bc14bca45644241
ninaghar23 <gharachorloo.ni@northeastern.edu>
2025-08-12 23:17:49 -0400

    3rd commit

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(
     origin: [
       "http://localhost:5173", // Local development
       "https://a5--superlative-figolla-b02519.netlify.app", 
-      'https://a6--superlative-figolla-b02519.netlify.app',// Your Netlify URL
+      "https://a6--superlative-figolla-b02519.netlify.app",
       process.env.NETLIFY_URL,
     ].filter(Boolean),
   })
